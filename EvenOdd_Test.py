cb8dd68840ada1f604119e0be4d0be3aba9f4647
Subhakanta Panda <47550887+PandaSubhakanta@users.noreply.github.com>
2021-09-09 13:20:31 +0530

    Update EvenOdd_Test.py

--- EvenOdd_Test.py
+++ EvenOdd_Test.py
@@ -1,6 +1,6 @@
 N={1,2,3,4,5,6,7,8,9,10} 
-    for i in N:     
-            if i%2==0:         
-                print('Even')     
-            else:         
-                print('Odd ')
+for i in N:        
+    if i%2==0:        
+        print('Even')    
+    else:         
+        print('Odd ')
